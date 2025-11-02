7fa0fb0e74e39e71cba3fc751d3505e4f5bb3ed9
Ky azize <kyazize4@gmail.com>
2025-11-02 22:41:14 +0000

    Mise à jour complète du serveur PeerJS pour Render

--- server.js
+++ server.js
@@ -10,8 +10,8 @@ const app = express();
 const corsOptions = {
     origin: [
         'https://pandurate-squatly-hae.ngrok-free.dev',
-        'https://visiocampus-socketio.onrender1.com',
-        'https://visio-sfu-server-4.onrender.com',
+        'https://visiocampus-socketio-1.onrender.com',
+        'https://visio-sfu-server-5.onrender.com',
         'http://localhost:3000',
         'http://localhost:8000',
         'http://localhost:5173'
